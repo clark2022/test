952914cc8dedef20f9a43df747f529b474bdb211
clark1990@me.com <clark1990@me.com>
2022-05-23 20:35:47 +0800

    echo get data

--- drf/backend/cfehome/api/views.py
+++ drf/backend/cfehome/api/views.py
@@ -1,6 +1,21 @@
+import json
+
 from django.http import JsonResponse
 
 # Create your views here.
 
-def api_home(request):
+def api_home(request,*args,**kwargs):
+
+    body=request.body
+    data={}
+    try:
+        data=json.loads(body)
+        data['params'] = dict(request.GET)
+        data['headers'] = dict(request.headers)
+        data['content_type'] = dict(request.content_type)
+    except:
+        pass
+    print(data)
+
+
     return JsonResponse ({'message':"hello,django api"})
--- drf/py_client/consumer.py
+++ drf/py_client/consumer.py
@@ -2,5 +2,8 @@ import  requests
 
 endpoint='http://127.0.0.1:8000/api/'
 
-resp=requests.get(endpoint)
+# resp=requests.get(endpoint)
+# print(resp.json()['message'])
+
+resp=requests.get(endpoint,params={'abc':123},json={"query":"hello world"})
 print(resp.json()['message'])
